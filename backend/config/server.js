737a29ab96f547eee7544e7a472a6bf2a6799535
Lmwet <lea.colson@gmail.com>
2020-10-20 11:27:37 +0200

    first

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '5328600ae6a08f79562077bf79190d26'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'ebbe5a5b-b1c7-4d23-b896-c3422d589adf'
+};
